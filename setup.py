4ce4a11f2ce762d6e3e21db584cafd63398186a7
Andreas Paepcke <paepcke@cs.stanford.edu>
2014-04-01 12:26:24 -0700

    In setup.py: bumped version to 0.27, and updated copy on PyPi.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import multiprocessing
 from setuptools import setup, find_packages
 setup(
     name = "online_learning_computations",
-    version = "0.26",
+    version = "0.27",
     packages = find_packages(),
 
     # Dependencies on other packages:
