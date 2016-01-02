76e06b1f3c0f027e67caf1eaa886d2d3f24ec7d7
Andreas Paepcke <paepcke@cs.stanford.edu>
2016-01-02 09:15:57 -0800

    Added data server, which publishes rows either from a CSV file, or from a db query. The latter yet untested.

--- src/learningviz/dataserver/dataserver.py
+++ src/learningviz/dataserver/dataserver.py
@@ -4,17 +4,19 @@ Created on Dec 31, 2015
 @author: paepcke
 '''
 
-import csv
-import itertools
 import argparse
-import sys
+import csv
+import getpass
+import itertools
 import os
+import sys
+import time
 
-from pymysql_utils import MySQLDB
+from pymysql_utils.pymysql_utils import MySQLDB
 
-from redis_bus_python import BusAdapter
+from redis_bus_python.redis_bus import BusAdapter
+from redis_bus_python.redis_lib.exceptions import ConnectionError 
 from redis_bus_python.bus_message import BusMessage
-
 
 class DataServer(object):
     '''
@@ -33,18 +35,34 @@ class DataServer(object):
     mysql_default_pwd  = ''
     mysql_default_db   = 'mysql'
     
+    inter_msg_delay    = 2 # second
 
-    def __init__(self):
-        self.bus = BusAdapter()
+    def __init__(self, topic, redis_server='localhost',):
+        super(DataServer,self).__init__()
+        try:
+            self.bus = BusAdapter(host=redis_server)
+        except ConnectionError:
+            print("Cannot connect to bus; is redis_bus running?")
+            sys.exit()
         self.colnames = []
+        self.topic = topic
         
-    def send_all(self, it):
-        for info in it:
-            bus_msg = BusMessage(content=self.make_json(info))
+    def send_all(self):
+        row_count = 0
+        print('Starting to publish data...')
+        for info in self.it:
+            if len(info) == 0:
+                # Empty line in CSV file:
+                continue
+            bus_msg = BusMessage(content=self.make_json(info),
+                                 topicName=self.topic)
             self.bus.publish(bus_msg)
+            row_count += 1
+            time.sleep(DataServer.inter_msg_delay)
+        print("Published %s data rows." % row_count)
     
     def make_json(self, content_line_arr):
-        self.colnames = self.invent_colnames(self.colnames)
+        self.colnames = self.invent_colnames(self.colnames, content_line_arr)
         return dict(itertools.izip_longest(self.colnames, content_line_arr, fillvalue='null'))
 
     def invent_colnames(self, existing_colnames, data_arr):
@@ -57,73 +75,183 @@ class DataServer(object):
 class MySQLDataServer(DataServer):
 
     def __init__(self, 
-                 query, 
+                 query,
+                 topic,
                  host=DataServer.mysql_default_host, 
                  port=DataServer.mysql_default_port, 
                  user=DataServer.mysql_default_user,
-                 passwd=DataServer.mysql_default_pwd, 
+                 pwd=DataServer.mysql_default_pwd, 
                  db=DataServer.mysql_default_db, 
-                 colname_arr=None):
+                 redis_server='localhost',
+                 colname_arr=[]):
         '''
         Constructor
         '''
-        super('MySQLDataServer', self).__init__()
-        self.db = MySQLDB(host=host, port=port, user=user, passwd=passwd, db=db)
+        # Super throws 'TypeError: must be type, not str'
+        # So need to use explicit superclass call...odd. 
+        #super('CSVDataServer', self).__init__(topic)
+        #super('MySQLDataServer', self).__init__(topic)
+        DataServer.__init__(self, topic)
+        self.db = MySQLDB(host=host, port=port, user=user, passwd=pwd, db=db, redis_server=redis_server)
         # Column name array to superclass instance:
         self.colnames = colname_arr
         self.it = self.db.query(query)
+        self.send_all()
         
 class CSVDataServer(DataServer):
     
     def __init__(self, 
                  fileName,
-                 first_line_has_colnames=False ):
-        super('MySQLDataServer', self).__init__()
+                 topic,
+                 first_line_has_colnames=False,
+                 redis_server='localhost',
+                 colnames=[] ):
+        # Super throws 'TypeError: must be type, not str'
+        # So need to use explicit superclass call...odd. 
+        #super('CSVDataServer', self).__init__(topic)
+        DataServer.__init__(self, topic, redis_server=redis_server)
                 
         fd = open(fileName, 'r')
         csvreader = csv.reader(fd)
-        if first_line_has_colnames:
-            # Column name array to superclass instance var:
-            self.colnames = csvreader.readline()
+        # Explicitly provided colnames have precedence
+        # over colnames in first line of file:
+        if len(colnames) > 0:
+            self.colnames = colnames
+            # Trash first line in file if appropriate:
+            if first_line_has_colnames:
+                csvreader.next()
+        elif first_line_has_colnames:
+            # No explicit col names, but file has them;
+            # send column name array to superclass instance var:
+            self.colnames = csvreader.next()
+        # ... else superclass will invent col names.
         self.it = csvreader
+        self.send_all()
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]), 
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-s', '--server',
-                        metavar='host', 
+                        dest='host', 
                         help="For mysql src: fully qualified db host name.",
                         default=DataServer.mysql_default_host)
-    parser.add_argument('-p', '--port',
-                        metavar='port', 
+    parser.add_argument('-o', '--port',
+                        dest='port', 
                         help="For mysql src: mysql server port.",
                         default=DataServer.mysql_default_port)
     parser.add_argument('-u', '--user',
-                        metavar='user', 
+                        dest='user', 
                         help="For mysql src: mysql user name.",
-                        default=DataServer.mysql_default_user)
+                        default=None)
     parser.add_argument('-p', '--pwd',
-                        metavar='pwd', 
+                        metavar='password',
+                        dest='pwd', 
                         help="For mysql src: mysql password.",
-                        default=DataServer.mysql_default_pwd)
+                        default=None)
     parser.add_argument('-d', '--db',
-                        metavar='db', 
+                        metavar='mysqldb',
+                        dest='db', 
                         help="For mysql src: mysql database to access.",
                         default=DataServer.mysql_default_db)
-    [Next: filename for csv, and first-line-colnames-y/n]
-    
-    parser.add_argument('source',
-                        metavar='source',
-                        help="Either 'csv' or 'mysql' to indicate where data is to be obtained.",
-    
-    parser.add_argument('-s', '--streamMsgs', 
-                        help="Send the same bus message over and over. Topic, or both,\n" +\
-                             "topic and content may be provided. If content is omitted,\n" +\
-                             "a random string of %d characters will be used.\n" % STANDARD_MSG_LENGTH +\
-                             "If topic is omitted as well, messages will be streamed to '%s'" % STREAM_TOPIC,
-                        dest='stream_topic_and_content',
+    parser.add_argument('-b', '--busserver',
+                        dest='redis_server', 
+                        help="Name of machine that serves the bus (redis server); default localhost.",
+                        default='localhost')
+    parser.add_argument('-t', '--type',
+                        metavar='csvOrDb',
+                        dest='csvOrDb',
+                        choices=['csv', 'mysql'],
+                        default='csv',
+                        required=True,
+                        help="Service type: 'csv' file or 'mysql' database; required.",
+                        )
+    parser.add_argument('-c', '--cols1stline',
+                        dest='colsIn1stLine',
+                        choices=['true', 'false'],
+                        default='false', 
+                        help="For csv src: true/false whether 1st line contains col names.",
+                        )
+    parser.add_argument('fileOrQuery',
+                        help="For mysql src: query to run; for csv: file name.",
+                       )
+    parser.add_argument('topic',
+                        help="Topic to which to publish.",
+                       )
+    parser.add_argument('colnames',
                         nargs='*',
-                        metavar=('topic', 'content'),
-                        default=None);
+                        metavar='columnnames', 
+                        help="Optional list of column names; for CSV may be provided in 1st line.",
+                       )
 
-        
+    args = parser.parse_args();
+
+    # Are we to serve CSV file, or a MySQL query:
+    src_type = args.csvOrDb
+    if src_type == 'csv':
+        filename = args.fileOrQuery
+    else:
+        query = args.fileOrQuery
+    colnames = args.colnames
+    
+    redis_server = args.redis_server
+    
+    # If serving a database query: Get all the security done:
+    if src_type == 'db':
+        host = args.host
+        port = args.port
+        db   = args.db
+        if args.user is None:
+            # Use current user as MySQL user:
+            user = getpass.getuser()
+        else:
+            user = args.user
+            
+        pwd = args.pwd
+        if pwd is not None and pwd.len == 0:
+            # Gave -p w/o arg; ask for pwd:
+            pwd = getpass.getpass("Enter %s's MySQL password on %s: " % (user,host))
+        elif pwd is None:
+            # -p given, but empty:
+            # Try to find pwd in specified user's $HOME/.ssh/mysql
+            currUserHomeDir = os.getenv('HOME')
+            if currUserHomeDir is None:
+                pwd = None
+            else:
+                # Don't really want the *current* user's homedir,
+                # but the one specified in the -u cli arg:
+                userHomeDir = os.path.join(os.path.dirname(currUserHomeDir), user)
+                try:
+                    # Need to access MySQL db as its 'root':
+                    with open(os.path.join(userHomeDir, '.ssh/mysql')) as fd:
+                        pwd = fd.readline().strip()
+                    
+                except IOError:
+                    # No .ssh subdir of user's home, or no mysql inside .ssh:
+                    pwd = None
+        # We now have all we need to serve a MySQL query
+    else:
+        # We are to serve a CSV file; does it exist?
+        try:
+            with open(filename, 'r') as fd:
+                pass
+        except IOError:
+            print("Could not open CSV file %s" % filename)
+            sys.exit()
+        colsIn1stLine = args.colsIn1stLine
+            
+    if src_type == 'csv':
+        server = CSVDataServer(filename,
+                               args.topic,
+                               first_line_has_colnames=colsIn1stLine,
+                               redis_server=redis_server,
+                               colnames=colnames)
+    else:
+        server = MySQLDataServer(query,
+                                 args.topic, 
+                                 host=host,
+                                 port=port,
+                                 user=user,
+                                 pwd=pwd,
+                                 db=db,
+                                 redis_server=redis_server,
+                                 colname_arr=colnames)
