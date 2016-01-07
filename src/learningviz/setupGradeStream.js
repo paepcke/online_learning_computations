6b2910abbee403732fb4d990acc8db61140d38bd
Andreas Paepcke <paepcke@cs.stanford.edu>
2016-01-07 09:07:29 -0800

    Started busstream.html and setupGradeStream.js.

---
+++ src/learningviz/setupGradeStream.js
@@ -0,0 +1,32 @@
+//console.log(vizzer);
+var gradeReceiver = function(gradeData) {
+	console.log(gradeData);
+}
+
+bus = busInteractor.getInstance();
+
+bus.setMsgCallback(gradeReceiver);
+bus.subscribeToTopic('gradesCompilers');
+//bus.subscribedTo();
+
+var testData = 
+			nextData = [
+					   {"course" : "Medicine/HRP258/Statistics_in_Medicine",
+					    "learner" : "6a6c70f0f9672ca4a3e16bdb5407af51cd18e4e5",
+					    "grade" : 10,
+					    "attempts" : 3,
+					    "firstSubmit" : "2013-06-11 15:12:13",
+					    "lastSubmit" : "2013-07-07 00:10:51",
+					    "probId" : "i4x://Medicine/HRP258/problem/5542da143b054d0ba3efdb243b5eb343"
+					   },
+					   {"course" : "Medicine/HRP258/Statistics_in_Medicine",
+					    "learner" : "cb2bb63c14e6f5fc8d21b5f43c8fe412c7c64c39",
+					    "grade" : 7,
+					    "attempts" : 1,
+					    "firstSubmit" : "2013-06-11 15:21:11",
+					    "lastSubmit" : "2013-07-1506:13:51",
+					    "probId" : "i4x://Medicine/HRP258/problem/8c13502687f642e1b514d4b522fc96d3",
+					   }
+		            ];
+
+
